23d2381914c71f3aa6da95d7a1eb49e002c31004
Pawel Owiesek <Owiesek.p@gmail.com>
2025-01-13 12:38:44 +0100

    Refactor Header component to use styled-components and enhance navigation links

--- src/header/index.js
+++ src/header/index.js
@@ -1,9 +1,25 @@
+import { Item, List, Logo, NavBar, StyledLink } from "./styled";
+
 export const Header = () => {
   return (
-    <ul>
-      <li>
-        <img src="favicon.ico" alt="logo"></img>
-      </li>
-    </ul>
+    <NavBar>
+      <List>
+        <Item>
+          <Logo src="favicon.ico" alt="logo" />
+        </Item>
+        <Item>
+          <StyledLink to="/">OnShopList</StyledLink>
+        </Item>
+        <Item>
+          <StyledLink to="/OnStockList">nStockList</StyledLink>
+        </Item>
+        <Item>
+          <StyledLink to="/SoldList">SoldList</StyledLink>
+        </Item>
+        <Item>
+          <StyledLink to="/SpecialOffer">SpecialOffer</StyledLink>
+        </Item>
+      </List>{" "}
+    </NavBar>
   );
 };
---
+++ src/header/styled.js
@@ -0,0 +1,39 @@
+import styled from "styled-components";
+import { NavLink } from "react-router-dom";
+export const NavBar = styled.header`
+  font-size: 23px;
+  font-weight: bold;
+  letter-spacing: 1.3px;
+  background-color: #b8b8b8;
+`;
+
+export const Logo = styled.img`
+  margin: 0;
+  padding-top: 5px;
+  width: 100px;
+  height: 80px;
+`;
+
+export const List = styled.ul`
+  margin: 0;
+  padding: 0;
+  list-style-type: none;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+`;
+
+export const Item = styled.li`
+  color: #ffffff;
+  cursor: pointer;
+`;
+export const StyledLink = styled(NavLink)`
+  color: white;
+  text-decoration: none;
+  border-bottom: 4px solid #b8b8b8;
+  padding-bottom: 4px;
+  &:hover {
+    border-bottom: 4px solid #671515;
+    color: crimson;
+  }
+`;
