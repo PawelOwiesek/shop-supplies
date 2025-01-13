35e7209998d0dab6748c82558757c08bdf32c39a
Pawel Owiesek <Owiesek.p@gmail.com>
2025-01-13 15:24:44 +0100

    Refactor OnShopList component to use styled-components for improved layout and styling

--- src/onShopList/index.js
+++ src/onShopList/index.js
@@ -1,16 +1,36 @@
+import {
+  ColumnName,
+  Container,
+  Divider,
+  Item,
+  List,
+  ProductData,
+} from "./styled";
+
 export const OnShopList = ({ products }) => {
   return (
-    <ul>
-      {products.map((product) => {
-        return (
-          <li key={product.name}>
-            <p>{product.name}</p>
-            <p> {product.brand}</p>
-            <p>{product.price} €</p>
-            <p>{product.quantity}</p>
-          </li>
-        );
-      })}
-    </ul>
+    <Container>
+      <List>
+        <ColumnName>
+          <p>Product</p>
+          <p>Brand</p>
+          <p>Price</p>
+          <p>Quantity</p>
+        </ColumnName>
+        {products.map((product) => {
+          return (
+            <>
+              <Item key={product.name}>
+                <ProductData>{product.name}</ProductData>
+                <ProductData> {product.brand}</ProductData>
+                <ProductData red>{product.price} €</ProductData>
+                <ProductData green>{product.quantity}</ProductData>
+              </Item>
+              <Divider />
+            </>
+          );
+        })}
+      </List>
+    </Container>
   );
 };
---
+++ src/onShopList/styled.js
@@ -0,0 +1,52 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  background-image: url(/logo.jpg);
+  background-repeat: no-repeat;
+  background-size: cover;
+  color: #201e1e;
+  font-size: 22px;
+  padding-top: 60px;
+`;
+
+export const List = styled.ul`
+  list-style-type: none;
+  margin: 0 auto;
+  padding: 0 0 20px 0;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: flex-start;
+  max-width: 1000px;
+  width: 100%;
+  background-color: #f3eaea;
+`;
+
+export const Item = styled.li`
+  display: flex;
+  gap: 40px;
+  margin-left: 20px;
+`;
+
+export const ProductData = styled.p`
+  font-weight: 700;
+  min-width: 300px;
+  min-width: ${({ red }) => (red ? "100px" : "")};
+  min-width: ${({ green }) => (green ? "100px" : "")};
+  width: 100%;
+  color: ${({ red }) => (red ? "crimson" : "")};
+  color: ${({ green }) => (green ? "green" : "")};
+`;
+
+export const ColumnName = styled.div`
+  display: grid;
+  grid-template-columns: repeat(2, 330px) repeat(2, 100px);
+  margin-left: 50px;
+  color: blue;
+  font-weight: bold;
+`;
+
+export const Divider = styled.div`
+  border: 2px solid #008080;
+  width: 100%;
+`;
