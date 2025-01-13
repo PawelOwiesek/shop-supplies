d7646ab3b33a55e72fdc1f288bd20bfa381ef4b3
Pawel Owiesek <Owiesek.p@gmail.com>
2025-01-13 14:28:52 +0100

    Add products data and update OnShopList component to display product details

--- src/App.js
+++ src/App.js
@@ -5,13 +5,14 @@ import { OnShopList } from "./onShopList";
 import { OnStockList } from "./onStockList";
 import { SoldList } from "./soldList";
 import { SpecialOffer } from "./specialOffer";
+import { products } from "./products";
 
 function App() {
   return (
     <div>
       <Header />
       <Routes>
-        <Route path="/" element={<OnShopList />} />
+        <Route path="/" element={<OnShopList products={products} />} />
         <Route path="/OnStockList" element={<OnStockList />} />
         <Route path="/SoldList" element={<SoldList />} />
         <Route path="/SpecialOffer" element={<SpecialOffer />} />
--- src/onShopList/index.js
+++ src/onShopList/index.js
@@ -1,3 +1,16 @@
-export const OnShopList = () => {
-  return <h1>nShopList</h1>;
+export const OnShopList = ({ products }) => {
+  return (
+    <ul>
+      {products.map((product) => {
+        return (
+          <li key={product.name}>
+            <p>{product.name}</p>
+            <p> {product.brand}</p>
+            <p>{product.price} €</p>
+            <p>{product.quantity}</p>
+          </li>
+        );
+      })}
+    </ul>
+  );
 };
