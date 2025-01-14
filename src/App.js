67e31e03d71c10c70fcd4a83da6c0905d8bb9ac9
Pawel Owiesek <Owiesek.p@gmail.com>
2025-01-14 12:19:48 +0100

    Implement product purchase functionality in App component

--- src/App.js
+++ src/App.js
@@ -6,13 +6,49 @@ import { OnStockList } from "./onStockList";
 import { SoldList } from "./soldList";
 import { SpecialOffer } from "./specialOffer";
 import { products } from "./products";
+import { useState } from "react";
+// import { v4 as uuidv4 } from "uuid";
 
 function App() {
+  const [active, setActive] = useState(false);
+  const [activeProduct, setActiveProduct] = useState("");
+  const [soldProducts, setSoldProducts] = useState([]);
+  const [updatedProductsList, setUpdatedProductLIst] = useState(products);
+
+  const onButtonBuyClick = (id) => {
+    const activeProduct = products.find((product) => product.id === id);
+    setActiveProduct(activeProduct);
+    setActive(true);
+    setSoldProducts([...soldProducts, activeProduct]);
+    setUpdatedProductLIst((prevList) => [
+      ...prevList.slice(0, id - 1),
+      {
+        ...prevList[id - 1],
+        quantity: prevList[id - 1].quantity - 1,
+      },
+      ...prevList.slice(id),
+    ]);
+  };
+  console.log(updatedProductsList);
   return (
     <div>
-      <Header />
+      <Header active={active} setActive={setActive} />
       <Routes>
-        <Route path="/" element={<OnShopList products={products} />} />
+        <Route
+          path="/"
+          element={
+            <OnShopList
+              products={products}
+              updatedProductsList={updatedProductsList}
+              active={active}
+              setActive={setActive}
+              onButtonBuyClick={onButtonBuyClick}
+              activeProduct={activeProduct}
+              soldProducts={soldProducts}
+              setSoldProducts={setSoldProducts}
+            />
+          }
+        />
         <Route path="/OnStockList" element={<OnStockList />} />
         <Route path="/SoldList" element={<SoldList />} />
         <Route path="/SpecialOffer" element={<SpecialOffer />} />
