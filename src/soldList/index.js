5307037597e54359b9527c3d70d7e50b2338b7ba
Pawel Owiesek <Owiesek.p@gmail.com>
2025-01-17 23:46:30 +0100

    Refactor TotalSoldList component to calculate and display total earnings with improved layout and styling

--- src/soldList/index.js
+++ src/soldList/index.js
@@ -1,15 +1,69 @@
+import { Container, Item, List, ProductData } from "../styledList/styledList";
+
 export const TotalSoldList = ({ totalSold }) => {
+  const initialValue = 0;
+  const totalEarnings = totalSold?.map((article) => {
+    return article.price * article.quantity;
+  }, initialValue);
+
+  const shopEarnings = totalEarnings?.reduce((acc, item) => {
+    return acc + (item || 0);
+  }, initialValue);
+
   return (
-    <h1>
-      {totalSold?.map((item) => {
-        return (
-          <li key={item.name}>
-            {item.name}-st:
-            {item.quantity}
-            sold for:{(item.price * item.quantity).toFixed(2)}
-          </li>
-        );
-      })}
-    </h1>
+    shopEarnings && (
+      <Container>
+        <>
+          {" "}
+          <h1 style={{ textAlign: "center", color: " #ffffff" }}>
+            Total sold articles
+          </h1>
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              gap: "250px",
+              textAlign: "center",
+              color: " #ffffff",
+              maxWidth: "1300px",
+              marginLeft: "120px",
+              fontWeight: "bold",
+              fontSize: "23px",
+            }}
+          >
+            {" "}
+            <p>Product</p>
+            <p>Quantity</p>
+            <p>Total price</p>
+          </div>
+          <List>
+            {totalSold?.map((item) => {
+              return (
+                <Item key={item.name}>
+                  <ProductData>{item.name}</ProductData>
+                  <ProductData> {item.quantity}</ProductData>
+                  <ProductData>
+                    {" "}
+                    sold for:{(item.price * item.quantity).toFixed(2)}
+                  </ProductData>
+                </Item>
+              );
+            })}
+          </List>
+          <p
+            style={{
+              backgroundColor: " #ffffff",
+              padding: "10px",
+              textAlign: "center",
+              alignSelf: "center",
+              maxWidth: "400px",
+              margin: "15px auto",
+            }}
+          >
+            Shop earnings : {shopEarnings.toFixed(2)} €
+          </p>
+        </>
+      </Container>
+    )
   );
 };
