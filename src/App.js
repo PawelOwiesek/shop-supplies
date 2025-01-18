f2d202f63c884238169b6d1ee5b3ef089a3e3318
Pawel Owiesek <Owiesek.p@gmail.com>
2025-01-18 01:34:02 +0100

    Refactor App component to improve structure and readability by removing unused state and simplifying JSX

--- src/App.js
+++ src/App.js
@@ -11,7 +11,6 @@ import { ShoppingCart } from "./shoppingCart";
 
 function App() {
   const [active, setActive] = useState(false);
-  const [activeProduct, setActiveProduct] = useState("");
   const [soldProducts, setSoldProducts] = useState([]);
   const [updatedProductsList, setUpdatedProductList] = useState(products);
   const [totalSold, setTotalSold] = useState([]);
@@ -41,7 +40,6 @@ function App() {
 
   const onButtonBuyClick = (id) => {
     const activeProduct = findActiveProduct(id);
-    setActiveProduct(activeProduct);
     openCart();
     addToSoldProducts(activeProduct);
     updateProductQuantity(id);
@@ -52,45 +50,43 @@ function App() {
   };
 
   return (
-    <div>
-      <>
-        <Header active={active} setActive={setActive} />
-        <Routes>
-          <Route
-            path="/"
-            element={
-              <OnShopList
-                updatedProductsList={updatedProductsList}
-                active={active}
-                onButtonBuyClick={onButtonBuyClick}
-              ></OnShopList>
-            }
-          />
-          <Route
-            path="/OnStockList"
-            element={
-              <OnStockList
-                updatedProductsList={updatedProductsList}
-                setUpdatedProductList={setUpdatedProductList}
-              />
-            }
-          />
-          <Route
-            path="/TotalSoldList"
-            element={<TotalSoldList totalSold={totalSold} />}
-          />
-          <Route path="/SpecialOffer" element={<SpecialOffer />} />
-        </Routes>
-        <ShoppingCart
-          active={active}
-          setActive={setActive}
-          soldProducts={soldProducts}
-          setSoldProducts={setSoldProducts}
-          soldStuff={soldStuff}
+    <>
+      <Header active={active} setActive={setActive} />
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <OnShopList
+              updatedProductsList={updatedProductsList}
+              active={active}
+              onButtonBuyClick={onButtonBuyClick}
+            ></OnShopList>
+          }
         />
-        <Footer />
-      </>
-    </div>
+        <Route
+          path="/OnStockList"
+          element={
+            <OnStockList
+              updatedProductsList={updatedProductsList}
+              setUpdatedProductList={setUpdatedProductList}
+            />
+          }
+        />
+        <Route
+          path="/TotalSoldList"
+          element={<TotalSoldList totalSold={totalSold} />}
+        />
+        <Route path="/SpecialOffer" element={<SpecialOffer />} />
+      </Routes>
+      <ShoppingCart
+        active={active}
+        setActive={setActive}
+        soldProducts={soldProducts}
+        setSoldProducts={setSoldProducts}
+        soldStuff={soldStuff}
+      />
+      <Footer />
+    </>
   );
 }
 
